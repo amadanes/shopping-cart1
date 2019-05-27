24e911c36da69cd24bbf683a279de06eefc9273e
amadanes <madanes@ivf.us>
2019-05-27 15:31:57 -0500

    shopping cart functionality completed

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -3,7 +3,12 @@
     <h1>Product List</h1>
     <img v-if="loading" src="https://i.imgur.com/JfPpwOA.gif">
     <ul v-else>
-      <li v-for="product in products">{{product.title}} - {{product.price}}</li>
+      <li v-for="product in products">
+        {{product.title}} - {{product.price}} available {{product.inventory}}
+        <button
+          @click="addProductToCart(product)"
+        >Add to cart</button>
+      </li>
     </ul>
   </div>
 </template>
@@ -21,6 +26,13 @@ export default {
       return this.$store.getters.availableProducts;
     }
   },
+
+  methods: {
+    addProductToCart(product) {
+      this.$store.dispatch("addProductToCart", product);
+    }
+  },
+
   created() {
     this.loading = true;
     this.$store.dispatch("fetchProducts").then(() => (this.loading = false));
--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,9 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     // = data
-    products: []
+    products: [],
+    cart: []
+    // will hold {id quantity}
   },
 
   getters: {
@@ -28,6 +30,21 @@ export default new Vuex.Store({
           resolve();
         });
       });
+    },
+
+    addProductToCart(context, product) {
+      if (product.inventory > 0) {
+        // find cartItem
+        const cartItem = context.state.cart.find(
+          item => item.id === product.id
+        );
+        if (!cartItem) {
+          context.commit("pushProductToCart", product.id);
+        } else {
+          context.commit("incrementItemQuantity", cartItem);
+        }
+        context.commit("decrementPeoductInventory", product);
+      }
     }
   },
 
@@ -35,6 +52,21 @@ export default new Vuex.Store({
     setProducts(state, products) {
       // update products
       state.products = products;
+    },
+
+    pushProductToCart(state, productId) {
+      state.cart.push({
+        id: productId,
+        quantity: 1
+      });
+    },
+
+    incrementItemQuantity(state, cartItem) {
+      cartItem.quantity++;
+    },
+
+    decrementPeoductInventory(state, product) {
+      product.inventory--;
     }
   }
 });
